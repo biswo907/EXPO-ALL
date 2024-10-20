f6560a5660299d66adf99c92926644b5ac911e72
Biswo <biswajitdash907@gmail.com>
2024-10-20 10:46:05 +0530

    ecommerce 1st commit

---
+++ E_Commerce/constants/Colors.ts
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary: '#572fff',
+  black: '#333',
+  gray: '#666',
+  lightGray: '#999',
+  white: '#fff',
+  background: '#F4F4F4',
+  highlight: '#F4CE14',
+}
+
+// npm install -g npm@10.9.0
