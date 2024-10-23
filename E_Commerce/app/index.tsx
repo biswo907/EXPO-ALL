e5c4bd71f2d0b0868d81cb1adffa5f62330c1416
biswo-squbix <biswajit.dash@squbix.com>
2024-10-23 09:51:00 +0530

    2nd commit

--- E_Commerce/app/index.tsx
+++ E_Commerce/app/index.tsx
@@ -1,24 +1,47 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
-import { Link } from "expo-router";
+import { Link, Stack } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
 
 type Props = {};
 
 const WelcomeScreen = (props: Props) => {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Screen</Text>
-      <Link href={"/signin"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignIn Screen</Text>
-        </TouchableOpacity>
-      </Link>
-      <Link href={"/signup"} asChild>
-        <TouchableOpacity>
-          <Text>Go to SignUp Screen</Text>
-        </TouchableOpacity>
-      </Link>
-    </View>
+    <>
+      <ImageBackground
+        source={require("../assets/images/ecommerce-splash.jpg")}
+        style={{ flex: 1 }}
+        resizeMode="cover"
+      />
+      <View style={styles.container}>
+        <LinearGradient
+          // Background Linear Gradient
+          colors={[
+            "rgba(255,255,255,0.9)",
+            "rgba(255,255,255,1)",
+            "transparent",
+          ]}
+          style={styles.background}
+        />
+        <Text>Welcome Screen</Text>
+        <Link href={"/signin"} asChild>
+          <TouchableOpacity>
+            <Text>Go to SignIn Screen</Text>
+          </TouchableOpacity>
+        </Link>
+        <Link href={"/signup"} asChild>
+          <TouchableOpacity>
+            <Text>Go to SignUp Screen.....</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+    </>
   );
 };
 
@@ -26,8 +49,18 @@ export default WelcomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    position: "absolute",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "red",
+    opacity: 0.5,
+  },
+  background: {
+    flex: 1,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
